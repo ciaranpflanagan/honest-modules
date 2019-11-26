c69adf60d9858240024d0f91ece5d3b085b1dd61
Ciaran Flanagan <ciaran@cicisoft.net>
2019-11-26 21:12:53 +0000

    Started module index page

--- app/Controllers/Http/ModuleController.js
+++ app/Controllers/Http/ModuleController.js
@@ -1,9 +1,13 @@
 'use strict'
+const Database = use('Database')
 
 class ModuleController {
 	// Shows Single Module Page
-	  index ({ request, response, view }) {
-	    return view.render('modules/index')
+	  async index ({ request, response, view, params }) {
+	  	const mod = await Database.from('modules')
+	  		.where('module_code', params.module_code)
+
+	    return view.render('modules/index', {mod: mod})
 	  }
 }
 
